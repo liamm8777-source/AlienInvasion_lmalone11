f47ddee9e952f626c827b34702186daf981a166a
liamm8777 <liamm8777@gmail.com>
2026-07-25 19:38:24 -0400

    Docs: Add required project header to main module

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,15 @@
+"""
+Program: Alien Invasion - Track 1
+Author: Liam Malone
+Purpose: Create a Pygame game with a ship that moves vertically and fires
+lasers horizontally at aliens.
+Date: 07/25/2026
+"""
+
 import sys
+
 import pygame
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     pass
